08a4706d5134f36a8adc5452e87dd8c1aeb486fa
rayzr522 <rayzr522@gmail.com>
2018-05-05 12:33:00 -0400

    Added base project

---
+++ src/main/java/me/rayzr522/matchmaker/Matchmaker.java
@@ -0,0 +1,17 @@
+package me.rayzr522.matchmaker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Matchmaker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
